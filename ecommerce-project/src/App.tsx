90cccb3e251da4a91259c0a390d6331127b3ffa4
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-08 14:57:21 -0300

    Add 404 not found page

--- ecommerce-project/src/App.tsx
+++ ecommerce-project/src/App.tsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
+import { NotFound404Page } from './pages/NotFound404Page';
 
 import './App.css'
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFound404Page />} />
     </Routes>
   )
 }
