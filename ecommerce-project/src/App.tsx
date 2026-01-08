fb6af827ddb3efff67233858b81babf0b9c1efed
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-08 09:24:33 -0300

    Move the home page into react

--- ecommerce-project/src/App.tsx
+++ ecommerce-project/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
+import { HomePage } from './pages/HomePage';
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>React App</p>
-    </>
+    <HomePage />
   )
 }
 
