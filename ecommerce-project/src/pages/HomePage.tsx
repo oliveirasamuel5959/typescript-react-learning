ae7dca8d92bed385abf6af8008d1af0c8b2e9262
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-09 15:13:14 -0300

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.tsx
+++ ecommerce-project/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { Button } from '../components/Button';
 import { products } from '../../starting-code/data/products';
@@ -6,22 +7,10 @@ import { useState } from 'react';
 
 
 function fetchData() {
-
-  fetch('http://localhost:5500/api/products')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Parse the response as JSON
-    })
-    .then(data => {
-      console.log(data);
-      // return data;
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
+  axios.get('http://localhost:5500/api/products')
+    .then((response) => {
+      console.log(response.data);
     });
-
 }
 
 export function HomePage() {
