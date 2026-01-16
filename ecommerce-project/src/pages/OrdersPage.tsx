2541e7c83ab4f3c1f8c3db7fde2f8ccfbf718dbf
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-16 09:22:07 -0300

    Correct orders page cart error

--- ecommerce-project/src/pages/OrdersPage.tsx
+++ ecommerce-project/src/pages/OrdersPage.tsx
@@ -3,12 +3,12 @@ import { Header } from '../components/Header';
 import BuyAgainIcon from '../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
 
-export function OrdersPage() {
+export function OrdersPage( {cart} ) {
   return (
     <>
       <title>Orders</title>
       <link rel="icon" type="image/png" href="orders-favicon.png" />
-      <Header />
+      <Header cart={cart}/>
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
