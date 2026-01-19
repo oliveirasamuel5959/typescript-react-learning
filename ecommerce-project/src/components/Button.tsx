dccfddf579ec24852c3a2be02d892f020c4344de
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-19 08:23:55 -0300

    Create add to cart feature

--- ecommerce-project/src/components/Button.tsx
+++ ecommerce-project/src/components/Button.tsx
@@ -1,12 +1,23 @@
-export function Button() {
+import axios from "axios";
+
+export function Button({ product }) {
 
   function handleSubmit() {
-    console.log("button working")
+    const postData = async () => {
+      const response = await axios.post('/api/cart-items', {
+        productId: product.id,
+        quantity: 1
+      });
+    };
+
+    postData();
   }
 
   return (
     <>
-      <button onClick={handleSubmit} className="add-to-cart-button button-primary">
+      <button className="add-to-cart-button button-primary"
+        onClick={handleSubmit} 
+      >
         Add to Cart
       </button>
     </>
--- ecommerce-project/src/pages/home/HomePage.tsx
+++ ecommerce-project/src/pages/home/HomePage.tsx
@@ -4,7 +4,7 @@ import { Header } from '../../components/Header';
 import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid';
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart}) {
 
   const [products, setProducts] = useState([]);
 
@@ -20,11 +20,12 @@ export function HomePage({ cart }) {
   return (
     <>
       <title>Ecommerce Project</title>
-      <link rel="icon" type="image/png" href="home-favicon.png" />
+      {/* <link rel="icon" type="image/png" href="home-favicon.png" /> */}
       <Header cart={cart} />
       <div className="home-page">
         <ProductsGrid 
           products={products}
+          loadCart={loadCart}
         />
       </div>
     </>
